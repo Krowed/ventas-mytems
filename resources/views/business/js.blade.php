35830cd450eedc6488fe81c267123018108dc6e7
Krowed <krowed17@gmail.com>
2025-12-26 06:42:06 -0500

    Cambios 26-12-25 maniana2

--- resources/views/business/js.blade.php
+++ resources/views/business/js.blade.php
@@ -1,8 +1,11 @@
 <script>
-
 let departamentoChoices;
 let provinciaChoices;
 let distritoChoices;
+
+let selDepartamento = document.querySelector('select[name="departamento"]'),
+    selProvincia   = document.querySelector('select[name="provincia"]'),
+    selDistrito    = document.querySelector('select[name="distrito"]');
 
 function load_ubigeo() {
     fetch("{{ route('admin.load_ubigeo') }}", {
@@ -10,101 +13,124 @@ function load_ubigeo() {
         headers: {
             'Content-Type': 'application/json',
             'X-CSRF-TOKEN': csrfToken
-        }
+        },
+        body: JSON.stringify({ }) // enviar body vacío para Laravel
     })
     .then(res => res.json())
     .then(r => {
-        let selDepartamento   = document.querySelector('select[name="departamento"]'),
-            selProvincia        = document.querySelector('select[name="provincia"]'),
-            selDistrito         = document.querySelector('select[name="distrito"]');
 
-        let wrapProvincia     = document.getElementById('wrapper_province'),
-            wrapDistrito        = document.getElementById('wrapper_district');
+        let wrapProvincia = document.getElementById('wrapper_province'),
+            wrapDistrito  = document.getElementById('wrapper_district');
 
-        let htmlDepartment      = '',
-            htmlProvince        = '',
-            htmlDistrict        = '';
+        // Reset HTML
+        selDepartamento.innerHTML = '';
+        selProvincia.innerHTML   = '';
+        selDistrito.innerHTML    = '';
 
-            if(r.ubigeo !== null) {
-                wrapProvincia.classList.remove('d-none');
-                wrapDistrito.classList.remove('d-none');
+        let htmlDepartment = `<option value="">[SELECCIONE]</option>`;
+        let htmlProvince   = `<option value="">[SELECCIONE]</option>`;
+        let htmlDistrict   = `<option value="">[SELECCIONE]</option>`;
 
-                r.departments.forEach(dep => {
-                    htmlDepartment += `<option value="${dep.codigo}" ${dep.codigo === r.department.codigo ? 'selected' : '' }>${dep.departamento}</option>`;
-                });
+        if (r.ubigeo !== null) {
+            wrapProvincia.classList.remove('d-none');
+            wrapDistrito.classList.remove('d-none');
 
-                r.provinces.forEach(prov => {
-                    htmlProvince += `<option value="${prov.codigo}" ${prov.codigo === r.province.codigo ? 'selected' : ''}>${prov.provincia}</option>`;
-                });
-
-                r.districts.forEach(dist => {
-                    htmlDistrict += `<option value="${dist.codigo}" ${dist.codigo === r.district.codigo ? 'selected' : ''}>${dist.distrito}</option>`;
-                });
-
-                selProvincia.innerHTML  = htmlProvince;
-                selDistrito.innerHTML   = htmlDistrict;
-            }
-            else {
-                htmlDepartment += `<option value="">[SELECCIONE]</option>`;
-                r.departments.forEach(dep => {
-                    htmlDepartment += `
-                        <option value="${dep.codigo}">
-                            ${dep.departamento}
-                        </option>`;
-                });
-
-                wrapProvincia.classList.add('d-none');
-                wrapDistrito.classList.add('d-none');
-            }
-
-            selDepartamento.innerHTML = htmlDepartment;
-
-            if (departamentoChoices) departamentoChoices.destroy();
-            departamentoChoices = new Choices(selDepartamento, {
-                placeholder: true,
-                placeholderValue: '[SELECCIONE]',
-                itemSelectText: ''
+            r.departments.forEach(dep => {
+                htmlDepartment += `<option value="${dep.codigo}"
+                    ${dep.codigo == r.department.codigo ? 'selected' : '' }>
+                    ${dep.departamento}
+                </option>`;
             });
 
-            if (provinciaChoices) provinciaChoices.destroy();
-            provinciaChoices = new Choices(selProvincia, {
-                itemSelectText: ''
+            r.provinces.forEach(prov => {
+                htmlProvince += `<option value="${prov.codigo}"
+                    ${prov.codigo == r.province.codigo ? 'selected' : '' }>
+                    ${prov.provincia}
+                </option>`;
             });
 
-            if (distritoChoices) distritoChoices.destroy();
-            distritoChoices = new Choices(selDistrito, {
-                itemSelectText: ''
+            r.districts.forEach(dist => {
+                htmlDistrict += `<option value="${dist.codigo}"
+                    ${dist.codigo == r.district.codigo ? 'selected' : '' }>
+                    ${dist.distrito}
+                </option>`;
             });
+        }
+        else {
+            // Solo departamentos
+            r.departments.forEach(dep => {
+                htmlDepartment += `<option value="${dep.codigo}">
+                    ${dep.departamento}
+                </option>`;
+            });
+
+            wrapProvincia.classList.add('d-none');
+            wrapDistrito.classList.add('d-none');
+        }
+
+        // Insert HTML
+        selDepartamento.innerHTML = htmlDepartment;
+        selProvincia.innerHTML   = htmlProvince;
+        selDistrito.innerHTML    = htmlDistrict;
+
+        // Inicializar Choices
+        if (departamentoChoices) departamentoChoices.destroy();
+        departamentoChoices = new Choices(selDepartamento, {
+            placeholder: true,
+            placeholderValue: '[SELECCIONE]',
+            itemSelectText: ''
+        });
+
+        if (provinciaChoices) provinciaChoices.destroy();
+        provinciaChoices = new Choices(selProvincia, {
+            placeholder: true,
+            placeholderValue: '[SELECCIONE]',
+            itemSelectText: ''
+        });
+
+        if (distritoChoices) distritoChoices.destroy();
+        distritoChoices = new Choices(selDistrito, {
+            placeholder: true,
+            placeholderValue: '[SELECCIONE]',
+            itemSelectText: ''
+        });
     })
     .catch(err => console.error(err));
 }
 
-let selectDeparment = document.querySelector('select[name="departamento"]');
-selectDeparment.addEventListener('change', function() {
+selDepartamento.addEventListener('change', function() {
     let value = this.value;
+
     fetch("{{ route('admin.load_provinces') }}", {
         method: "POST",
         headers: {
             'Content-Type': 'application/json',
             'X-CSRF-TOKEN': csrfToken
         },
-        body: JSON.stringify({
-            codigo: value
-        })
+        body: JSON.stringify({ codigo: value })
     })
     .then(res => res.json())
     .then(r => {
-        let htmlProvince        = '',
-            selProvincia        = document.querySelector('select[name="provincia"]');
+        selProvincia.innerHTML = `<option value="">[SELECCIONE]</option>`;
+        selDistrito.innerHTML  = `<option value="">[SELECCIONE]</option>`;
 
-        console.log(r.provinces);
         r.provinces.forEach(prov => {
-            htmlProvince += `<option value="${prov.codigo}">${prov.provincia}</option>`;
+            selProvincia.innerHTML += `
+                <option value="${prov.codigo}">
+                    ${prov.provincia}
+                </option>`;
         });
 
-        selProvincia.innerHTML = htmlProvince;
         if (provinciaChoices) provinciaChoices.destroy();
         provinciaChoices = new Choices(selProvincia, {
+            placeholder: true,
+            placeholderValue: '[SELECCIONE]',
+            itemSelectText: ''
+        });
+
+        // Limpia distrito
+        if (distritoChoices) distritoChoices.destroy();
+        distritoChoices = new Choices(selDistrito, {
             placeholder: true,
             placeholderValue: '[SELECCIONE]',
             itemSelectText: ''
